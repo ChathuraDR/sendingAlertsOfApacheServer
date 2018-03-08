35e6d9e32c07fc482702c1e34b7a32580880541c
Chathura D Ranathunga <Chathurad92@gmail.com>
2018-03-08 10:34:37 +0530

    Add files via upload

---
+++ checkLatestAccess.py
@@ -0,0 +1,59 @@
+import smtplib
+
+def validate_ip(s):
+    a = s.split('.')
+    if len(a) != 4:
+        return False
+    for x in a:
+        if not x.isdigit():
+            return False
+        i = int(x)
+        if i < 0 or i > 255:
+            return False
+    return True
+
+def sndMail(mssg):
+
+    gmail_user = "xxxxxxxxxxx@gmail.com"
+    gmail_password = "xxxxxxxxxxxxxxxxx"
+
+    fromAddr = gmail_user
+    toAddr = "xxxxxxxxxxxxx@gmail.com"
+    subject = "Apache server new alert !"
+    body = mssg
+
+    email_text = """\  
+    From: %s  
+    To: %s  
+    Subject: %s
+
+    %s
+    """ % (fromAddr, toAddr, subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(fromAddr, toAddr, email_text)
+        server.close()
+
+        print ("Email sent!")
+    except:
+        print ("Something went wrong...")
+
+infile = "/var/log/apache2/access.log"
+oldMsg = ""
+
+while True:
+    with open(infile) as myfile:
+        lstLine = list(myfile)[-1]
+        ip = (lstLine.split(" ")[0])
+
+        if(validate_ip(ip)):
+            tmp = lstLine.split(" ")[3]
+            tmp = tmp.split(":")[0] + ":" + tmp.split(":")[1] + ":" + tmp.split(":")[2]
+            newMsg = ip + " Time : " + tmp + "]"
+            if ( newMsg != oldMsg ):
+                sndMail(newMsg)
+            oldMsg = newMsg
+
